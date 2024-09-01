4892e9748fc864d092c911755b4355cb18085136
Rodrigo Nakata <153712774+nakatadev@users.noreply.github.com>
2024-09-01 14:43:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 window.onload = function() {
     setTimeout(function () {
-        window.location.href = "https://produto.mercadolivre.com.br/MLB-4588098980-kit-3-camisetas-masculina-lisa-basica-algodo-tradicional-_JM";
+        window.location.href = "https://produto.mercadolivre.com.br/MLB-2835427916-kit-5-camisetas-proteco-solar-camisa-uv-malha-fria-889an4-_JM";
     }, 1500);
 };
